936d81ea20e64841d70a5fd57532e717040f629f
amir <amir@email.com>
2021-12-18 13:05:18 +0330

    add ep6

--- home/views.py
+++ home/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 
+
 def say_hello(request):
-	return render(request, 'hello.html')
+	return render(request, 'hello.html', {'name':'jack'})
