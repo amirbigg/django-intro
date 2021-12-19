4d9a3ddc65edcdeb5631cb33916e2eee13d6c954
amir <amir@email.com>
2021-12-19 11:19:29 +0330

    add ep7

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	created = models.DateTimeField()
