2eb62f3e049cf21eac7a60497aaca2405703d1d9
amir <amir@email.com>
2021-12-29 12:45:28 +0330

    add ep21

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path('register/', views.user_register, name='user_register'),
 	path('login/', views.user_login, name='login'),
+	path('logout/', views.user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def user_register(request):
@@ -36,3 +36,9 @@ def user_login(request):
 	else:
 		form = UserLoginForm()
 	return render(request, 'login.html', {'form':form})
+
+
+def user_logout(request):
+	logout(request)
+	messages.success(request, 'logged out successfully', 'success')
+	return redirect('home')
