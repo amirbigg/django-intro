3daf8f21ad470014fa3e0dfc971da15025500b93
amir <amir@email.com>
2021-12-31 11:31:58 +0330

    add ep22

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('register/', views.user_register, name='user_register'),
-	path('login/', views.user_login, name='login'),
-	path('logout/', views.user_logout, name='logout'),
+	path('login/', views.user_login, name='user_login'),
+	path('logout/', views.user_logout, name='user_logout'),
 ]
